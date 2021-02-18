be72aa75618175c7fcec61ab6dcad34f74cf8206
Lindsay Deaton <lindsay-deaton@lambdastudents.com>
2021-02-18 11:12:04 -0500

    back to compiling

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,24 @@
 // scheme-model
+const db = require("../../data/db-config.js")
+
+module.exports = {
+  find,
+  // findById,
+  // findSteps,
+  // add,
+  // update,
+  // remove
+}
+function find() {
+  return db("schemes")
+}
+
+// function findByID(id) {
+//   return db("schemes")
+//     .where({ id })
+//     .first()
+// }
+
+// function 
+
+// function update(changes, id)
